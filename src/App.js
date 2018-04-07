404d938c7d8ccda4db04148ecd92dc7f3edf60c1
Charles <charlesclei21@gmail.com>
2018-04-07 02:51:59 -0600

    Login menu works now, I hope

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
+import md5 from 'crypto-md5';
 import HeaderApp from './components/HeaderApp.js';
 import StockBrowser from './containers/StockBrowser.js';
 import SingleUser from './containers/SingleUser.js';
@@ -7,6 +8,11 @@ import Home from './containers/Home.js';
 import SingleStock from "./containers/SingleStock.js";
 import StockVisualizer from "./containers/StockVisualizer.js";
 import AboutUs from "./containers/AboutUs.js";
+import axios from 'axios';
+
+//Redirect tutorial and code grabbed from https://reacttraining.com/react-router/web/example/auth-workflow
+//Router OnEnter function has been depreciated since V3 and installing it via npm installed the latest V4 version
+//Redirect was the new way to do front end authentication, so code and tutorial was copied from the tutorial site
 
 import {
     
@@ -27,7 +33,6 @@ const fakeAuth = {
     setTimeout(cb, 100);
   }
 };
-
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -91,20 +96,56 @@ class App extends Component {
 
 //Login Class to redirect components to
 class LoginPage extends Component {
+ constructor() {
+     super();
+    this.state = {
+        redirectToReferrer: false,
+        emailInput: '',
+        passwordInput: ''
+      };
+        
+     this.email = "";
+         this.handleLogin = this.handleLogin.bind(this);
+         this.handleChangeEmail = this.handleChangeEmail.bind(this);
+         this.handleChangePassword = this.handleChangePassword.bind(this);
+     }
+  
 
-       state = {
-    redirectToReferrer: false
+  handleLogin(event) {
+      
+      let saltedPass;
+    event.preventDefault();
+     
+    axios.get('https://bestdatabasev2.herokuapp.com/api/users/'+this.state.emailInput).then(response => { 
+        
+            
+            saltedPass = this.state.passwordInput.concat(response.data[0].salt);
+            saltedPass = md5(saltedPass, 'hex');
+        if (saltedPass === response.data[0].password) {
+            fakeAuth.authenticate(() => {
+                    this.setState({ redirectToReferrer: true });
+                });
+        }
+            console.log(saltedPass);
+        })
+        .catch(function (error) {
+            alert('Error with api call ... error=' + error);
+        });      
+    
   };
 
-  login = () => {
-    fakeAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true });
-    });
-  };
+    handleChangeEmail(event) {    
+        this.setState({emailInput: event.target.value});
+        
+      }
+    
+    handleChangePassword(event) {    
+        this.setState({passwordInput: event.target.value});
+        
+      }
 
 //Login page template created from https://github.com/dansup/bulma-templates/blob/master/templates/login.html
  render() {
-
 
  const { from } = this.props.location.state || { from: { pathname: "/" } };
     const { redirectToReferrer } = this.state;
@@ -120,15 +161,15 @@ class LoginPage extends Component {
           <h3 className="title has-text-grey">BitStocker Login</h3>
           <p className="subtitle has-text-grey">Please enter login information</p>
           <div className="box">
-            <form>
+            <form onSubmit={this.handleLogin}>
               <div className="field">
                 <div className="control">
-                  <input className="input is-large" type="email" placeholder="Your Email" autoFocus="" />
+                  <input className="input is-large" type="email" onChange={this.handleChangeEmail} placeholder="Your Email" autoFocus="" />
                 </div>
               </div>
               <div className="field">
                 <div className="control">
-                  <input className="input is-large" type="password" placeholder="Your Password" />
+                  <input className="input is-large" type="password" onChange={this.handleChangePassword} placeholder="Your Password" />
                 </div>
               </div>
               <button className="button is-block is-info is-large is-fullwidth">Login</button>
@@ -140,7 +181,7 @@ class LoginPage extends Component {
             
             <div>
         <p>You must log in to view the page at {from.pathname}</p>
-        <button onClick={this.login}>Log in</button>
+        <button>Log in</button>
       </div>
   </section>
          
